e09d118b6a8be168720e8afa64acd310b1feced8
Dange1TheMange1 <agerskovcasper@gmail.com>
2024-10-15 10:23:59 +0200

    Set up the test level again, and also lined out the logic for the new door system.

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -2,12 +2,28 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    public void SwitchOpenState()
+    // WILL MAKE ANIMATIONS WHEN WE GET DOOR ASSETS
+    // WILL MAKE SOUND WHEN SOUND MANAGER IS PRESENT
+    
+    //Legacy code, could not be used for switch states puzzle
+    /*public void SwitchOpenState()
     {
         if (gameObject.activeInHierarchy)
         {
             gameObject.SetActive(false);
         }else
             gameObject.SetActive(true);
+    }*/
+
+    public void Open()
+    {
+        //Play animation
+        //Play sound
+    }
+
+    public void Close()
+    {
+        //Play animation
+        //Play sound
     }
 }
