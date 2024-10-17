28ff0090843fd64615d4a5444329cb429f1affd7
BingoMango <103103876+Dange1TheMange1@users.noreply.github.com>
2024-10-17 10:24:17 +0200

    Puzzles on GrayBox

--- Assets/Clicker.cs
+++ Assets/Clicker.cs
@@ -4,8 +4,25 @@ using UnityEngine.Events;
 public class Clicker : MonoBehaviour
 {
     [SerializeField] UnityEvent Event;
+    [Space]
+    [SerializeField] GameObject KeyHolder;
+    [SerializeField] GameObject Key;
 
-    public void sexualStyle() { 
-        Event.Invoke();
+    public void sexualStyle() {
+        if (Key != null)
+        {
+            if (Key.transform.parent == KeyHolder.transform)
+            {
+                Event.Invoke();
+            }
+            else
+            {
+                //display some error message
+            }
+        }
+        else 
+        {
+            Event.Invoke();
+        }
     }
 }
--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -5,18 +5,19 @@ public class DoorScript : MonoBehaviour
     // WILL MAKE ANIMATIONS WHEN WE GET DOOR ASSETS
     // WILL MAKE SOUND WHEN SOUND MANAGER IS PRESENT
     
-    //Legacy code, could not be used for switch states puzzle
-    /*public void SwitchOpenState()
+    //Legacy code, could be used for switch states puzzle
+    public void SwitchOpenState()
     {
         if (gameObject.activeInHierarchy)
         {
             gameObject.SetActive(false);
         }else
             gameObject.SetActive(true);
-    }*/
+    }
 
     public void Open()
     {
+        gameObject.SetActive(false); //slå det her ihjel senere
         //Play animation
         //Play sound
     }
