ffd7676a8645eede0a258d50cac8ea6473a8cafb
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2024-10-28 14:45:09 +0100

    Audio Test

---
+++ Assets/Audio/Audio Scripts/AudioScriptTest.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using JSAM;
+
+public class AudioScriptTest : MonoBehaviour
+{
+    public KeyCode test_Key;
+    public SoundFileObject test;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(test_Key))
+        {
+            AudioManager.PlaySound(test);
+            Debug.Log("This sound " + test + " is playing");
+        }
+    }
+}
