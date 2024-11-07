2846e96436dc44df0d87ea610682189c7226727c
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2024-11-07 15:39:10 +0100

    #34  the start menu have been createde with loadning screen and the visual part of the data logger but the rest is need

--- Assets/Scripts/MainMenu/MainMenuLogic.cs
+++ Assets/Scripts/MainMenu/MainMenuLogic.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Collections;
+using UnityEngine.UI;
 public class MainMenuLogic : MonoBehaviour
 {
     public TMP_InputField datalogName_InputField;
+    public TMP_Text loadningScreenLog;
+    public Slider loadningSlider;
+    public GameObject debugMenu, MainMenu, LoadningScreen;
+    public string loadninglogText = "Loading progress: ", lastlog= "Loadning the last stuff";
     public void loadScene(int num)
     {
-        SceneManager.LoadScene(num); //set this to something we decide when building
+        StartCoroutine(asynchronousLoad(num));
     }
 
     public void exitApplication() {
@@ -22,4 +28,57 @@ public class MainMenuLogic : MonoBehaviour
     public void createNewDataloggerCollection() {
         Debug.Log("Create a new datalogger collection");
     }
+    int loadningDotNumber = 1;
+    public int maxLoadningDots = 3;
+    IEnumerator asynchronousLoad(int num)
+    {
+        debugMenu.SetActive(false);
+        MainMenu.SetActive(false);
+        LoadningScreen.SetActive(true);
+
+        float elapsedTime = 0f;
+        string log = loadninglogText + "0% (0s)";
+        loadningScreenLog.SetText(log);
+        loadningSlider.value = 0;
+        yield return null;
+        AsyncOperation ao = SceneManager.LoadSceneAsync(num);
+        ao.allowSceneActivation = false;
+        yield return null;
+        while (!ao.isDone)
+        {
+            
+            if (ao.progress < 0.9f )
+            {
+                elapsedTime += Time.deltaTime;
+                float realProgress = Mathf.Clamp01(ao.progress / 0.9f);
+                float simulatedProgress = Mathf.Lerp(0f, 1f, elapsedTime / 10f);
+                float displayProgress = Mathf.Max(realProgress, simulatedProgress);
+                int progressProcent = (int)(displayProgress * 100);
+
+                loadningSlider.value = progressProcent;
+                log = $"{loadninglogText}{progressProcent}% ({Mathf.FloorToInt(elapsedTime)}s)";
+                loadningScreenLog.SetText(log);
+            }
+            else {
+                log = lastlog;
+                for (int i = 0; i < loadningDotNumber; i++)
+                {
+                    log += ".";
+                }
+                loadningScreenLog.SetText(lastlog);
+                loadningDotNumber++;
+                if (loadningDotNumber > maxLoadningDots)
+                {
+                    loadningDotNumber = 1;
+                }
+                ao.allowSceneActivation = true;
+                yield return null;
+            }
+            yield return null;
+        }
+    }
+
+
+
+
 }
