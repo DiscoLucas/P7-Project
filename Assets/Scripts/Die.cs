23b6de700d7ea960c67644901cc07b2efd85499c
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2024-11-11 09:47:21 +0100

    Created Die class

---
+++ Assets/Scripts/Die.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnAttack()
+    {
+        Debug.Log("Die OnAttack");
+    }
+}
