f5c64b761171d7c4b79d34b2348e46b44074cc51
BingoMango <103103876+Dange1TheMange1@users.noreply.github.com>
2024-11-11 14:51:20 +0100

    Im gonna leave the lever handling for now

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -3,17 +3,37 @@ using UnityEngine.Events;
 
 public class Clicker : MonoBehaviour
 {
+    public bool switched;
     [SerializeField] UnityEvent Event;
+    [SerializeField] UnityEvent EventOpp;
     [Space]
     [SerializeField] GameObject KeyHolder;
     [SerializeField] GameObject Key;
+    [SerializeField] Animation MyAnimation;
 
-    public void sexualStyle() {
+    public void SwitchState(){
+        if(switched){
+            switched=false;
+        }else{
+            switched=true;
+        }
+    }
+
+    public void sexualStyle() { //This is like a button, that is pressed once and then Stuff A happens. We need it to become like a lever, where you switch it and Stuff A happens. But if you switch it again the opposite of Stuff A happens.
         if (Key != null)
         {
             if (Key.transform.parent == KeyHolder.transform)
             {
+                if(switched){
                 Event.Invoke();
+                }else{
+                EventOpp.Invoke();
+                }
+
+                if (MyAnimation != null)
+                {
+                    MyAnimation.Play();
+                }
             }
             else
             {
@@ -22,7 +42,16 @@ public class Clicker : MonoBehaviour
         }
         else 
         {
+            if(switched){
             Event.Invoke();
+            }else{
+            EventOpp.Invoke();
+            }
+
+            if (MyAnimation != null)
+            {
+                MyAnimation.Play();
+            }
         }
     }
 }
