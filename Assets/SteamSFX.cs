9d4c6dfb6caa7988620babfac30d441b8c37ef26
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2024-11-18 13:48:59 +0100

    Tutorial audio fix still too loud

--- Assets/SteamSFX.cs
+++ Assets/SteamSFX.cs
@@ -4,15 +4,22 @@ using JSAM;
 public class SteamSFX : MonoBehaviour
 {
     public SoundFileObject steam;
+    public SoundFileObject valve;
     public Transform Transform;
+    public Transform valvePosition;
 
     private void Start()
     {
-        AudioManager.PlaySound(steam, Transform.position);
+        AudioManager.PlaySound(steam, transform);
     }
 
     public void disableSteamSFX()
     {
         AudioManager.StopSound(steam);
+    }
+
+    public void playValveSound()
+    {
+        AudioManager.PlaySound(valve, valvePosition);
     }
 }
