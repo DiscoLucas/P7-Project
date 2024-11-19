de87f357a3b6e8f98d81d3d94873a41878e4d5e3
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2024-11-19 15:31:18 +0100

    Fixed the player and #49

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -44,8 +44,45 @@ public class GameManager : SingletonPersistent<GameManager>
     {
        GameManager.Instance.onGameStart();
     }
-    public void onGameStart()
-    {
+
+    public void findMonster() {
+        if (monsterObject == null)
+        {
+            GameObject[] monsters = GameObject.FindGameObjectsWithTag(monsterTag);
+            Debug.Log("mONSTERS FOUND: " + monsters.Length);
+            foreach (GameObject monster in monsters)
+            {
+                if (usingNextBot)
+                {
+                    DumbBot bot = monster.GetComponent<DumbBot>();
+                    if (bot != null)
+                    {
+                        Debug.Log("Nextbot found");
+                        monsterObject = monster;
+                    }
+                    else
+                    {
+                        monster.SetActive(false);
+                    }
+                }
+                else
+                {
+                    MonsterBrain bot = monster.GetComponent<MonsterBrain>();
+                    if (bot != null)
+                    {
+                        Debug.Log("GoapBot found");
+                        monsterObject = monster;
+                    }
+                    else
+                    {
+                        monster.SetActive(false);
+                    }
+                }
+            }
+            Debug.Log("monster gameobject name " + monsterObject.name);
+        }
+    }
+    public void onGameStart(){
         Debug.Log("Game startede");
         if(protectionAreaObject == null)
             protectionAreaObject = GameObject.FindGameObjectWithTag(eggTag);
@@ -122,39 +159,7 @@ public class GameManager : SingletonPersistent<GameManager>
             initialFilmGrain = filmGrain.intensity.value;
         }
         */
-        if (monsterObject == null) {
-            GameObject[] monsters = GameObject.FindGameObjectsWithTag(monsterTag);
-            Debug.Log("mONSTERS FOUND: " + monsters.Length);
-            foreach (GameObject monster in monsters)
-            {
-                if (usingNextBot)
-                {
-                    DumbBot bot = monster.GetComponent<DumbBot>();
-                    if (bot != null)
-                    {
-                        Debug.Log("Nextbot found");
-                        monsterObject = monster;
-                    }
-                    else
-                    {
-                        monster.SetActive(false);
-                    }
-                }
-                else
-                {
-                    MonsterBrain bot = monster.GetComponent<MonsterBrain>();
-                    if (bot != null)
-                    {
-                        Debug.Log("GoapBot found");
-                        monsterObject = monster;
-                    }
-                    else
-                    {
-                        monster.SetActive(false);
-                    }
-                }
-            }
-        }
+        findMonster();
         Debug.Log("Protection areas object is: " + protectionAreaObject + " Player is: " + playerObject + " Monster is: " + monsterObject);
         onGameStartEvent.Invoke();
     }
@@ -220,8 +225,13 @@ public class GameManager : SingletonPersistent<GameManager>
 
     private void HandleStarting()
     {
-
-        monsterObject.SetActive(true);
+        if (monsterObject != null)
+            monsterObject.SetActive(true);
+        else {
+            Debug.LogError("The monster was not found");
+            findMonster();
+            monsterObject.SetActive(true);
+        }
         if (SessionLogTracker.Instance.state == null) {
             SessionLogTracker.Instance.state = GameObject.FindObjectOfType<SessionLogState>();
         }
