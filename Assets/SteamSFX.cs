ed1278a7a5a58aca6b8d369a0309b2f8a1a9043d
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2024-11-19 10:44:53 +0100

    Animation Change and Valve sfx fix

--- Assets/SteamSFX.cs
+++ Assets/SteamSFX.cs
@@ -10,7 +10,7 @@ public class SteamSFX : MonoBehaviour
 
     private void Start()
     {
-        AudioManager.PlaySound(steam, transform);
+        AudioManager.PlaySound(steam, Transform);
     }
 
     public void disableSteamSFX()
